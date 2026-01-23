1a4306fa6ed2f381c4e200de3c84b582fecc6888
waghh781@gmail.com <waghh781@gmail.com>
2026-01-24 00:44:21 +0530

    Work on navbar

--- portfulio-react/src/App.jsx
+++ portfulio-react/src/App.jsx
@@ -1,8 +1,8 @@
 import Navbar from "./components/Navbar.jsx";
-import Home from "./components/Home";
+import Home from "./components/Home.jsx";
 import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+import Skills from "./components/Skills.jsx";
+import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact"
 function App() {
  
--- portfulio-react/src/main.jsx
+++ portfulio-react/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx';
 import './style.css';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
