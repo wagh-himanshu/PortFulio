ff2bd7c86aa24ed2ce847fd180d518ef3a639db7
waghh781@gmail.com <waghh781@gmail.com>
2026-02-08 21:40:07 +0530

    updated project

--- portfulio-react/src/components/Home.jsx
+++ portfulio-react/src/components/Home.jsx
@@ -2,7 +2,7 @@ function Home(){
 
       return(
             <section className="section home">
-                  <h1>Hi, I'm Himanshu 👋</h1>
+                  <h1>Hi, I'm Himanshu 👋 fksnjskj kfjgkajkjk ja jkjjfk fsjksfjkh bfjkhjkfh </h1>
       <p>Computer Science Engineering Student | React Developer</p>
 
       <a href="https://github.com/wagh-himanshu" target="_blank">
